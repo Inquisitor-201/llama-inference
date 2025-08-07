3eb235d3af1aac71dea5e8b964f5378c5f81a80f
Inquisitor-201 <1365605830@qq.com>
2025-08-07 10:53:07 +0800

    Add tokenizer configuration and initial story content
    - Created a new tokenizer configuration file for TinyStories-656K with special tokens for story start and end.
    - Added a sample story to output.txt featuring a girl named Lily and her adventure with a bird.
    - Included a prompt in prompt.txt to initiate the story.

---
+++ main.py
@@ -0,0 +1,18 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# tokenizer = AutoTokenizer.from_pretrained("raincandy-u/TinyStories-656K")
+# model = AutoModelForCausalLM.from_pretrained("raincandy-u/TinyStories-656K")
+
+tokenizer = AutoTokenizer.from_pretrained("./models/TinyStories-656K")
+model = AutoModelForCausalLM.from_pretrained("./models/TinyStories-656K")
+
+# Generate text
+prompt = "One day, a girl named "
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+
+# generated_ids = model.generate(input_ids=input_ids, max_length=100, do_sample=True, top_p=0.95, top_k=60)
+generated_ids = model.generate(input_ids=input_ids, max_length=100, do_sample=False, top_p=0.95, top_k=60)
+generated_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+
+print(generated_text)
